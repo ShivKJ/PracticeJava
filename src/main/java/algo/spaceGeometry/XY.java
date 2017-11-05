6019203342d8115d7cd11b4a39d3abdb3a4947fb
ShivKJ <shivkrishna.jaiswal@delhivery.com>
2017-11-05 08:59:03 +0530

    minor change in implementation of XY class

--- src/main/java/algo/spaceGeometry/XY.java
+++ src/main/java/algo/spaceGeometry/XY.java
@@ -4,26 +4,24 @@ import org.apache.commons.math3.ml.clustering.Clusterable;
 
 public class XY extends AbstractPoint implements Clusterable {
 
-	private final double x , y;
+	private final double[] xy;
 
 	public XY(double x, double y) {
-		this.x = x;
-		this.y = y;
+		this.xy = new double[] { x, y };
 	}
 
 	@Override
 	public double X() {
-		return x;
+		return xy[0];
 	}
 
 	@Override
 	public double Y() {
-		return y;
+		return xy[1];
 	}
 
 	@Override
 	public double[] getPoint() {
-
-		return new double[] { x, y };
+		return xy.clone();
 	}
 }
